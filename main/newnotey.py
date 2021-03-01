15af0cef34c5b4a2ff5475b08f0c310131bb2092
mallesh622 <78818829+mallesh622@users.noreply.github.com>
2021-03-01 18:49:49 -0500

    perfect branch checking

---
+++ main/newnotey.py
@@ -0,0 +1,4 @@
+this is the change in the main 
+
+this is for the brranch rel_1.0
+rel_1.0 let's checkout  this for last time
---
+++ test/newnote.py
@@ -0,0 +1,3 @@
+this is for the brranch rel_1.0
+
+rel_1.0 is what i'm checking 
